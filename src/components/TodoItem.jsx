289434e4f3bd349f03f59fa815f10130101aa3e0
Ellissea Montes <montes.ellissea.pasillos@gmail.com>
2025-11-29 16:04:12 +0800

    Implement TodoItem  component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,25 +1,24 @@
-import { useDispatch } from "react-redux"
-import { updateTodo, deleteTodo } from "../features/todos/todosSlice"
+// src/components/TodoItem.jsx
+import React from "react";
 
-function TodoItem ({ todo }) {
-  const dispatch = useDispatch()
-
-  const toggleComplete = () => {
-    dispatch(updateTodo({ ...todo, completed: !todo.completed }))
-  }
-
-  const handleDelete = () => {
-    dispatch(deleteTodo(todo.id));
-  }
-return (
-    <li>
-      <input type="checkbox" checked={todo.completed} onChange={toggleComplete} />
-      <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-        {todo.title}
-      </span>
-      <button onClick={handleDelete}>Delete</button>
-    </li>
-  )
+export default function TodoItem({ todo, index, toggleComplete, deleteTodo, theme }) {
+  return (
+    <div
+      className={`todo ${theme}-todo ${todo.completed ? "completed" : ""}`}
+    >
+      <li className="todo-item">{todo.text}</li>
+      <button
+        className={`check-btn ${theme}-button`}
+        onClick={() => toggleComplete(index)}
+      >
+        ✔
+      </button>
+      <button
+        className={`delete-btn ${theme}-button`}
+        onClick={() => deleteTodo(index)}
+      >
+        🗑
+      </button>
+    </div>
+  );
 }
-
-export default TodoItem
